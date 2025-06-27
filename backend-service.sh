def1429026dcd739ca0f0079503ee279d36f0f57
Niharika2211 <107017109+Niharika2211@users.noreply.github.com>
2025-06-27 18:57:59 +0530

    Update backend-service.sh

--- backend-service.sh
+++ backend-service.sh
@@ -29,7 +29,7 @@ cat <<EOF > $SERVICE_FILE
 Description=Backend Service
 
 [Service]
-User=mini
+User=expense
 Environment="DB_HOST=${DB_HOST}"
 Environment="DB_USER=${DB_USER}"
 Environment="DB_PASSWORD=${DB_PASSWORD}" 
